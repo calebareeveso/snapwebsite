623e307e7c2043545aeca70ad7b87e260eb7b49e
caleb-007 <calebareeveso7@gmail.com>
2023-03-04 18:50:48 +0000

    feat: rm sharp

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 const express = require("express"),
   app = express(),
   puppeteer = require("puppeteer");
-const sharp = require("sharp");
 app.get("/", async (request, response) => {
-  try {
-    const browser = await puppeteer.launch({
-      args: ["--no-sandbox"],
-    });
-    const page = await browser.newPage();
-    //Set View port
-    await page.setViewport({ width: 1280, height: 720 });
-    // Read url query parameter.
-    await page.goto(request.query.url);
-    // Take snapshot
-    const snapshot = await page.screenshot();
-    // Close browser
-    await browser.close();
-
-    // Resize snapshot
-    const buffer = await sharp(snapshot)
-      .resize({ width: 320, height: 180 })
-      .jpeg()
-      .toBuffer();
-
-    // Send response
+  if (request.query.url == undefined) {
     response.setHeader("content-type", "text/plain");
-    response.send(buffer.toString("base64"));
-  } catch (error) {
-    response.send({ error: error.message });
-    console.log(error);
+    response.send("ERROR: Missing URL parameter");
+  } else {
+    try {
+      const browser = await puppeteer.launch({
+        args: ["--no-sandbox"],
+      });
+      const page = await browser.newPage();
+      //Set View port
+      await page.setViewport({ width: 1280, height: 720 });
+
+      // Page Load Options
+      const pageLoadOptions = {
+        waitUntil: "load",
+      };
+
+      // Read url query parameter.
+      await page.goto(request.query.url, pageLoadOptions);
+
+      // Take snapshot
+      const b64string = await page.screenshot({ encoding: "base64" });
+
+      // Close browser
+      await browser.close();
+
+      // Send response
+      console.log(`Snapped: ${request.query.url}`);
+      response.setHeader("content-type", "text/plain");
+      response.send(b64string);
+    } catch (error) {
+      response.send({ error: error.message });
+      console.log(error);
+    }
   }
 });
 
