beb83ff26329b1bb96e788e9c2c76cbf09c064a8
caleb-007 <calebareeveso7@gmail.com>
2023-03-04 20:04:29 +0000

    feat: add deviceScaleFactor

--- index.js
+++ index.js
@@ -12,11 +12,16 @@ app.get("/", async (request, response) => {
       });
       const page = await browser.newPage();
       //Set View port
-      await page.setViewport({ width: 1280, height: 1280 }); // old height: 720
+      await page.setViewport({
+        width: 1280,
+        height: 1280,
+        deviceScaleFactor: 0.2,
+      }); // old height: 720
 
       // Page Load Options
       const pageLoadOptions = {
         waitUntil: "load",
+        timeout: 0,
       };
 
       // Read url query parameter.
